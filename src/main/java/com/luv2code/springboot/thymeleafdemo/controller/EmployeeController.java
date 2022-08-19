cd94de7db19c38730e89f3819e7a2f9a741401e5
asad <asadzahid1479@gmail.com>
2022-08-19 21:40:54 +0500

    implement update and delete methods

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
@@ -47,6 +44,19 @@ public class EmployeeController {
 		return "employees/employee-form";
 	}
 
+	@PostMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+
+		Employee theEmployee = employeeService.findById(theId);
+
+		// set employee as a model attribute to pre-populate the form
+		theModel.addAttribute("employee", theEmployee);
+
+		// send over to our form
+		return "employees/employee-form";
+	}
+
+
 	@PostMapping("/save")
 	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
 
@@ -55,6 +65,17 @@ public class EmployeeController {
 
 		// use a redirect to prevent duplicate submissions
 		return "redirect:/employees/list";
+	}
+
+	@PostMapping("/delete")
+	public String delete(@RequestParam("employeeId") int theId) {
+
+		// delete the employee
+		employeeService.deleteById(theId);
+
+		// redirect to /employees/list
+		return "redirect:/employees/list";
+
 	}
 }
 
